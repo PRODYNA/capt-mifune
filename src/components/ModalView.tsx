0e2e4fad39140f258d64981c112fba9b0ecdc486
Nico  Puhlmann <puhlmann7@googlemail.com>
2021-05-07 14:50:04 +0200

    restructured components

--- src/components/ModalView.tsx
+++ src/components/ModalView.tsx
@@ -1,16 +1,15 @@
-import {createStyles, makeStyles, Modal, Theme} from "@material-ui/core";
-import {ReactComponentElement, useState} from "react";
+import { createStyles, makeStyles, Modal, Theme } from "@material-ui/core";
+import { ReactComponentElement, useState } from "react";
 
 interface ModelProps {
-  open: boolean
-  content: ReactComponentElement<any>
-  onClose: () => void
+  open: boolean;
+  content: ReactComponentElement<any>;
+  onClose: () => void;
 }
-
 
 function getModalStyle() {
   const top = 50;
-  const left = 50 ;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -20,26 +19,24 @@ function getModalStyle() {
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      paper: {
-        position: 'absolute',
-        width: 600,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-      },
-    }),
+  createStyles({
+    paper: {
+      position: "absolute",
+      width: 600,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  })
 );
 
-
 export const ModalView = (props: ModelProps) => {
-
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
 
-  return  <Modal
-
+  return (
+    <Modal
       open={props.open}
       onClose={props.onClose}
       disableAutoFocus={true}
@@ -47,10 +44,10 @@ export const ModalView = (props: ModelProps) => {
       disableRestoreFocus={true}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
-  >
-    <div style={modalStyle} className={classes.paper}>
-      {props.content}
-    </div>
-  </Modal>
-
-}
+    >
+      <div style={modalStyle} className={classes.paper}>
+        {props.content}
+      </div>
+    </Modal>
+  );
+};
