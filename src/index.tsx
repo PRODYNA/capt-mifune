ccee17b21a95079902044acda999376e16af2fa8
Qin Li <qin.li@prodyna.com>
2021-09-16 16:01:50 +0200

    Add keycloak login

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HttpService from "./services/HttpService";
+import UserService from "./services/UserService";
 
 export async function loadEnv(): Promise<void> {
-  await fetch('/env.json')
-  .then(r => r.json())
-  .then(
-      env => {
-        Object.keys(env).forEach((k)=>{
-          localStorage.setItem(k, env[k])
-        })
-      }
-  );
+  await fetch("/env.json")
+    .then((r) => r.json())
+    .then((env) => {
+      Object.keys(env).forEach((k) => {
+        localStorage.setItem(k, env[k]);
+      });
+    });
 }
 
-loadEnv()
-.then(() => {
-  ReactDOM.render(
+const renderApp = () => {
+  loadEnv().then(() => {
+    ReactDOM.render(
       <React.StrictMode>
         <App />
       </React.StrictMode>,
-      document.getElementById('root')
-  );
-});
+      document.getElementById("root")
+    );
+  });
+};
 
-
+UserService.initKeycloak(renderApp);
+HttpService.configure();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
