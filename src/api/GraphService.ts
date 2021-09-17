319d535fd108d6ea62c44f15beb386f62948ba3f
Qin Li <qin.li@prodyna.com>
2021-09-17 11:42:04 +0200

    Refactor requests

--- src/api/GraphService.ts
+++ src/api/GraphService.ts
@@ -14,6 +14,8 @@ import {
 } from "./model/Model";
 import { AxiosResponse } from "axios";
 
+const rest = HttpService.getAxiosClient();
+
 export class GraphService {
   data(
     domainId: String,
@@ -21,7 +23,7 @@ export class GraphService {
     orders: String[] = [],
     filters: String[] = []
   ): Promise<any[]> {
-    return HttpService.getAxiosClient()
+    return rest
       .get<any[]>("data/domain/" + domainId, {
         params: { results: results, orders: orders, filters: filters },
       })
@@ -31,115 +33,99 @@ export class GraphService {
   }
 
   domainGet(id: string): Promise<Domain> {
-    return HttpService.getAxiosClient()
-      .get<Domain>("graph/domain/" + id)
-      .then((res) => {
-        console.log("domainGet response", res.data);
-        return res.data;
-      });
+    return rest.get<Domain>("graph/domain/" + id).then((res) => {
+      console.log("domainGet response", res.data);
+      return res.data;
+    });
   }
 
   domainsGet(): Promise<Domain[]> {
-    return HttpService.getAxiosClient()
-      .get<Domain[]>("graph/domains")
-      .then((res) => {
-        console.log("domainsGet response", res.data);
-        return res.data;
-      });
+    return rest.get<Domain[]>("graph/domains").then((res) => {
+      console.log("domainsGet response", res.data);
+      return res.data;
+    });
   }
 
   domainPost(model: DomainCreate): Promise<Domain> {
     console.log("domainPost model", model);
-    return HttpService.getAxiosClient()
-      .post<Domain>("graph/domain", model)
-      .then((res) => {
-        console.log("domainPost response: ", res.data);
-        return res.data;
-      });
+    return rest.post<Domain>("graph/domain", model).then((res) => {
+      console.log("domainPost response: ", res.data);
+      return res.data;
+    });
   }
 
   domainPut(id: string, model: DomainUpdate): Promise<Domain> {
     console.log("domainPut model", model);
-    return HttpService.getAxiosClient()
-      .put<Domain>("graph/domain/" + id, model)
-      .then((res) => {
-        console.log("domainPut response", res.data);
-        return res.data;
-      });
+    return rest.put<Domain>("graph/domain/" + id, model).then((res) => {
+      console.log("domainPut response", res.data);
+      return res.data;
+    });
   }
 
   domainDelete(id: string): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .delete<GraphDelta>("graph/domain/" + id)
       .then((res) => res.data);
   }
   domainClear(id: string): Promise<String> {
-    return HttpService.getAxiosClient()
+    return rest
       .delete<String>("graph/domain/" + id + "/clear")
       .then((res) => res.data);
   }
 
   domainImport(id: string): Promise<String> {
-    return HttpService.getAxiosClient()
+    return rest
       .get<String>("graph/domain/" + id + "/import")
       .then((res) => res.data);
   }
 
   graphGet(): Promise<Graph> {
-    return HttpService.getAxiosClient()
-      .get<Graph>("graph")
-      .then((r) => {
-        console.log("graphGet response", r.data);
-        return r.data;
-      });
+    return rest.get<Graph>("graph").then((r) => {
+      console.log("graphGet response", r.data);
+      return r.data;
+    });
   }
 
   nodePost(node: NodeCreate): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .post<GraphDelta>("graph/node", this.cleanNode(node))
       .then((r) => r.data);
   }
 
   nodePut(node: Node): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .put<GraphDelta>("graph/node/" + node.id, this.cleanNode(node))
       .then((r) => r.data);
   }
 
   nodeDelete(id: string): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
-      .delete<GraphDelta>("graph/node/" + id)
-      .then((res) => res.data);
+    return rest.delete<GraphDelta>("graph/node/" + id).then((res) => res.data);
   }
 
   relationPost(model: RelationCreate): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .post<GraphDelta>("graph/relation", this.cleanRelations(model))
       .then((r) => r.data);
   }
 
   relationPut(model: Relation): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .put<GraphDelta>("graph/relation/" + model.id, this.cleanRelations(model))
       .then((r) => r.data);
   }
 
   relationDelete(id: string): Promise<GraphDelta> {
-    return HttpService.getAxiosClient()
+    return rest
       .delete<GraphDelta>("graph/relation/" + id)
       .then((res) => res.data);
   }
 
   loadDefaultMappingConfig(domain: Domain): Promise<AxiosResponse<any>> {
-    return HttpService.getAxiosClient().get(
-      "graph/domain/" + domain.id + "/mapping"
-    );
+    return rest.get("graph/domain/" + domain.id + "/mapping");
   }
 
   loadQueryKeys(domain: Domain): Promise<string[]> {
-    return HttpService.getAxiosClient()
-      .get("data/domain/" + domain.id + "/keys")
-      .then((r) => r.data);
+    return rest.get("data/domain/" + domain.id + "/keys").then((r) => r.data);
   }
 
   cleanNode(node: NodeCreate | NodeUpdate): NodeCreate | NodeUpdate {
@@ -166,9 +152,7 @@ export class GraphService {
   }
 
   persistGraph(): Promise<any> {
-    return HttpService.getAxiosClient()
-      .post<GraphDelta>("graph")
-      .then((r) => r.data);
+    return rest.post<GraphDelta>("graph").then((r) => r.data);
   }
 }
 
