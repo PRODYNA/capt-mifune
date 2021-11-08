b2a09aec598d2b4d3f0f9d17351d07f17e54612c
coding4kay <landeck@web.de>
2021-11-08 13:32:49 +0100

    add clean database

--- src/components/form/FormSelect.tsx
+++ src/components/form/FormSelect.tsx
@@ -2,40 +2,40 @@ import * as React from "react";
 import { Form } from "react-bootstrap";
 
 interface IFormSelectProps {
-  title: string;
-  options: string[];
-  value?: string;
-  onChangeHandler?: (event: any) => void;
+    title: string;
+    options: string[];
+    value?: string;
+    onChangeHandler?: (event: any) => void;
 }
 
 const FormSelect: React.FunctionComponent<IFormSelectProps> = ({
-  title,
-  options,
-  value,
-  onChangeHandler,
+    title,
+    options,
+    value,
+    onChangeHandler,
 }) => {
-  const handleChange = (event: React.ChangeEvent) => {
-    if (onChangeHandler) {
-      onChangeHandler(event);
-    }
-  };
+    const handleChange = (event: React.ChangeEvent) => {
+        if (onChangeHandler) {
+            onChangeHandler(event);
+        }
+    };
 
-  return (
-    <Form.Group controlId="exampleForm.ControlSelect1" key={title}>
-      <Form.Label>{title}</Form.Label>
-      <Form.Control onChange={handleChange} as="select" value={value}>
-        {options.map((option: string) => {
-          return (
-            <option
-              key={option}
-              value={option !== "" ? option : undefined}
-              label={option !== "" ? option : "None"}
-            />
-          );
-        })}
-      </Form.Control>
-    </Form.Group>
-  );
+    return (
+        <Form.Group controlId="exampleForm.ControlSelect1" key={title}>
+            <Form.Label>{title}</Form.Label>
+            <Form.Control onChange={handleChange} as="select" value={value}>
+                {options.map((option: string) => {
+                    return (
+                        <option
+                            key={option}
+                            value={option !== "" ? option : undefined}
+                            label={option !== "" ? option : "None"}
+                        />
+                    );
+                })}
+            </Form.Control>
+        </Form.Group>
+    );
 };
 
 export default FormSelect;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -9,13 +9,13 @@ const _kc = new (Keycloak as any)("keycloak.json");
  * @param onAuthenticatedCallback
  */
 const initKeycloak = (onAuthenticatedCallback: () => void) => {
-  _kc
-    .init({
-      onLoad: "login-required",
-    })
-    .then(() => {
-      onAuthenticatedCallback();
-    });
+    _kc
+        .init({
+            onLoad: "login-required",
+        })
+        .then(() => {
+            onAuthenticatedCallback();
+        });
 };
 
 const doLogin = _kc.login;
@@ -27,21 +27,21 @@ const getToken = () => _kc.token;
 const isLoggedIn = () => !!_kc.token;
 
 const updateToken = (successCallback: () => Promise<AxiosRequestConfig>) =>
-  _kc.updateToken(5).then(successCallback).catch(doLogin);
+    _kc.updateToken(5).then(successCallback).catch(doLogin);
 
 const loginRequired = (): boolean =>
-  // Strings should be normalized to uppercase.
-  // A small group of characters, when they are converted to lowercase, cannot make a round trip.
-  localStorage.getItem("LOGIN_REQUIRED")?.toUpperCase() === "TRUE";
+    // Strings should be normalized to uppercase.
+    // A small group of characters, when they are converted to lowercase, cannot make a round trip.
+    localStorage.getItem("LOGIN_REQUIRED")?.toUpperCase() === "TRUE";
 
 const UserService = {
-  initKeycloak,
-  doLogin,
-  doLogout,
-  isLoggedIn,
-  getToken,
-  updateToken,
-  loginRequired,
+    initKeycloak,
+    doLogin,
+    doLogout,
+    isLoggedIn,
+    getToken,
+    updateToken,
+    loginRequired,
 };
 
 export default UserService;
