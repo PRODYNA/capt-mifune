e432fc9a355f13ab89a7ae6636cfcb3d84bceb66
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-12 09:37:12 +0100

    extract routes, highlight current navItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import "./App.css";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Graph } from "./components/graph/Graph";
-import { Pipelines } from "./components/pipeline/Pipelines";
-import { PipelinesDetail } from "./components/pipeline/PipelinesDetail";
-import { Analytics } from "./components/analytics/Analytics";
-import { Box, Container } from "@material-ui/core";
-import Sidenavigation from "./components/Navigation/SideNavigation";
-import Upload from "./pages/Upload";
+import { BrowserRouter } from "react-router-dom";
+import MifuneRouter from "./routes/Router";
 
 function App() {
   return (
-    <Router>
-      <Sidenavigation />
-      <Container>
-        <Box ml={5}>
-          <Switch>
-            <Route path="/" exact component={Graph} />
-            <Route path="/analytics" component={Analytics} />
-            <Route path="/upload" component={Upload} />
-            <Route path="/pipelines" component={Pipelines} />
-            <Route path="/pipeline/:id" component={PipelinesDetail} />
-          </Switch>
-        </Box>
-      </Container>
-    </Router >
+    <BrowserRouter>
+      <MifuneRouter />
+    </BrowserRouter >
   );
 }
 
---
+++ src/routes/routes.tsx
@@ -0,0 +1,7 @@
+const basePath =
+  document.getElementsByTagName('base')?.[0]?.getAttribute('href') || '/'
+export const ROOT_PATH = basePath
+export const ANALYTCIS = `${ROOT_PATH}analytics`
+export const UPLOAD = `${ROOT_PATH}upload`
+export const PIPELINES = `${ROOT_PATH}pipelines`
+console.log(basePath)
