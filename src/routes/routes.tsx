95bdd0d9afceb62b7a3fbe14e1e0748107ccddd8
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-12 10:49:16 +0100

    fix routing for detailpage pipeline

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,4 +4,5 @@ export const ROOT_PATH = basePath
 export const ANALYTCIS = `${ROOT_PATH}analytics`
 export const UPLOAD = `${ROOT_PATH}upload`
 export const PIPELINES = `${ROOT_PATH}pipelines`
+export const PIPELINE = `${ROOT_PATH}pipeline`
 console.log(basePath)
