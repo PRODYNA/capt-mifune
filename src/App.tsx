ccd30ae86428fe74829a0fd92207c479b566278d
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-12 09:39:32 +0100

    rename Router file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import MifuneRouter from "./routes/Router";
+import MifuneRouter from "./routes/MifuneRouter";
 
 function App() {
   return (
