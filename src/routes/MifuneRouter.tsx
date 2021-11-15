810d4f57911e9bc6540baa3b7de6709d0d3f42f0
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-15 13:49:04 +0100

    add react context for snackbar to show custom messages

--- src/routes/MifuneRouter.tsx
+++ src/routes/MifuneRouter.tsx
@@ -1,11 +1,12 @@
-import { Container, Box } from '@material-ui/core'
 import React from 'react'
+import { Container, Box } from '@material-ui/core'
 import { Switch, Route } from 'react-router-dom'
 import { Analytics } from '../components/analytics/Analytics'
 import { Graph } from '../components/graph/Graph'
 import Sidenavigation from '../components/Navigation/SideNavigation'
 import { Pipelines } from '../components/pipeline/Pipelines'
 import { PipelinesDetail } from '../components/pipeline/PipelinesDetail'
+import SnackbarProvider from '../context/Snackbar'
 import Upload from '../pages/Upload'
 import { ANALYTCIS, PIPELINE, PIPELINES, ROOT_PATH, UPLOAD } from './routes'
 
@@ -14,27 +15,29 @@ const MifuneRouter = (): JSX.Element => {
   return (
     <>
       <Sidenavigation />
-      <Container>
-        <Box ml={5}>
-          <Switch>
-            <Route path={ROOT_PATH} exact>
-              <Graph />
-            </Route>
-            <Route path={ANALYTCIS}>
-              <Analytics />
-            </Route>
-            <Route path={UPLOAD}>
-              <Upload />
-            </Route>
-            <Route path={PIPELINES} exact>
-              <Pipelines />
-            </Route>
-            <Route path={`${PIPELINE}/:id`}>
-              <PipelinesDetail />
-            </Route>
-          </Switch>
-        </Box>
-      </Container>
+      <SnackbarProvider>
+        <Container>
+          <Box ml={5}>
+            <Switch>
+              <Route path={ROOT_PATH} exact>
+                <Graph />
+              </Route>
+              <Route path={ANALYTCIS}>
+                <Analytics />
+              </Route>
+              <Route path={UPLOAD}>
+                <Upload />
+              </Route>
+              <Route path={PIPELINES} exact>
+                <Pipelines />
+              </Route>
+              <Route path={`${PIPELINE}/:id`}>
+                <PipelinesDetail />
+              </Route>
+            </Switch>
+          </Box>
+        </Container>
+      </SnackbarProvider>
     </>
   )
 }
