fd648c325539d4b3a2faa696bf20c2b7b07467d5
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-17 11:51:49 +0100

    add Dialogs and user feedback to pipelines

--- src/pages/pipeline/PipelineRow.tsx
+++ src/pages/pipeline/PipelineRow.tsx
@@ -3,10 +3,10 @@ import { Domain } from "../../api/model/Model";
 import graphService from "../../api/GraphService";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import WarningIcon from "@material-ui/icons/Warning";
+import ClearIcon from '@material-ui/icons/Clear';
 import DoneIcon from "@material-ui/icons/Done";
 import { IconButton, TableCell, TableRow } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import DeleteIcon from "@material-ui/icons/Delete";
 import StopIcon from "@material-ui/icons/Stop";
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import { useTheme } from '@material-ui/core/styles';
@@ -22,7 +22,7 @@ interface IPipelineRow {
 const PipelineRow = (props: IPipelineRow) => {
   const { domain, cleanActive, setShowProgress } = props
   const history = useHistory();
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>();
   const { openSnackbar, openSnackbarError } = useContext(SnackbarContext)
   const theme = useTheme()
 
@@ -31,7 +31,7 @@ const PipelineRow = (props: IPipelineRow) => {
     sseClient.onmessage = function (e) {
       setMessage(e.data);
     };
-
+    
     return function cleanUp() {
       sseClient.close();
     };
@@ -61,40 +61,42 @@ const PipelineRow = (props: IPipelineRow) => {
       .catch((e) => openSnackbarError(e))
   }
 
-  const deleteDomain = (): void => {
+  const clearDomain = (): void => {
     graphService.domainClear(domain.id)
-      .then(() => openSnackbar(Translations.DOMAIN_DELETE, 'success'))
+      .then(() => openSnackbar(Translations.DOMAIN_CLEAR, 'success'))
       .catch((e) => openSnackbarError(e))
   }
 
   return (
-    <TableRow key={domain.id}>
-      <TableCell align="left" onClick={() => history.push("/pipeline/" + domain.id)}>
-        <IconButton>
-          <VisibilityIcon htmlColor={theme.palette.grey[700]} />
-        </IconButton>
-      </TableCell>
-      <TableCell align="left">{domain.name}</TableCell>
-      <TableCell align="center">{valid(domain.modelValid)}</TableCell>
-      <TableCell align="center">{valid(domain.mappingValid)}</TableCell>
-      <TableCell align="center">
-        <IconButton disabled={cleanActive} onClick={runImport}>
-          <PlayArrowIcon htmlColor={theme.palette.primary.main} />
-        </IconButton>
-      </TableCell>
-      <TableCell align="center">
-        <IconButton onClick={stopImport}>
-          <StopIcon htmlColor={theme.palette.info.main} />
-        </IconButton>
-      </TableCell>
-      <TableCell align="center">
-        <IconButton onClick={deleteDomain }>
-          <DeleteIcon htmlColor={theme.palette.error.main} />
-        </IconButton>
-      </TableCell>
-      <TableCell align="left">{message}</TableCell>
-      <TableCell align="left">{domain.id}</TableCell>
-    </TableRow >
+    <>
+      <TableRow key={domain.id}>
+        <TableCell align="left" onClick={() => history.push("/pipeline/" + domain.id)}>
+          <IconButton>
+            <VisibilityIcon htmlColor={theme.palette.grey[700]} />
+          </IconButton>
+        </TableCell>
+        <TableCell align="left">{domain.name}</TableCell>
+        <TableCell align="center">{valid(domain.modelValid)}</TableCell>
+        <TableCell align="center">{valid(domain.mappingValid)}</TableCell>
+        <TableCell align="center">
+          <IconButton disabled={cleanActive} onClick={runImport}>
+            <PlayArrowIcon htmlColor={theme.palette.primary.main} />
+          </IconButton>
+        </TableCell>
+        <TableCell align="center">
+          <IconButton onClick={stopImport}>
+            <StopIcon htmlColor={theme.palette.info.main} />
+          </IconButton>
+        </TableCell>
+        <TableCell align="center">
+          <IconButton onClick={clearDomain}>
+            <ClearIcon htmlColor={theme.palette.error.main} />
+          </IconButton>
+        </TableCell>
+        <TableCell align="left">{message}</TableCell>
+        <TableCell align="left">{domain.id}</TableCell>
+      </TableRow >
+    </>
   );
 };
 
