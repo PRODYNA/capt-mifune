08a1a147e5aed2dd67b30c7f96ae000c83a68dc2
My Trang Hong <mytrang.hong@prodyna.com>
2021-11-18 12:33:51 +0100

    add snackbar to save pipelineEdit

--- src/pages/pipeline/PipelineEdit.tsx
+++ src/pages/pipeline/PipelineEdit.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useEffect, useState } from 'react'
+import React, { FormEvent, useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Grid } from '@material-ui/core'
 import { Domain, Source } from '../../api/model/Model'
@@ -6,6 +6,8 @@ import graphService from '../../api/GraphService'
 import FormSelect from '../../components/Form/FormSelect'
 import FormActions from '../../components/Form/FormActions'
 import HttpService from '../../services/HttpService'
+import { SnackbarContext } from '../../context/Snackbar'
+import { Translations } from '../../utils/Translations'
 
 const rest = HttpService.getAxiosClient()
 
@@ -16,6 +18,7 @@ interface DomainEditProps {
 const PipelineEdit = (props: DomainEditProps): JSX.Element => {
   const { domain } = props
   const history = useHistory()
+  const { openSnackbar, openSnackbarError } = useContext(SnackbarContext)
   const [mapping, setMapping] = useState<any>(domain.columnMapping ?? {})
   const [file, setFile] = useState<string | undefined>(domain.file)
   const [sources, setSources] = useState<Source[]>([])
@@ -111,7 +114,11 @@ const PipelineEdit = (props: DomainEditProps): JSX.Element => {
             file,
             columnMapping: mapping,
           })
-          .then(() => history.goBack())
+          .then(() => {
+            openSnackbar(Translations.SAVE, 'success')
+            history.goBack()
+          })
+          .catch((e) => openSnackbarError(e))
         event.preventDefault()
       }}
     >
