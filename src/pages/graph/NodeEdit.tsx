859f0fa99b4876b74d3e50e9574be7b13cab815d
My Trang Hong <mytrang.hong@prodyna.com>
2022-02-15 16:03:34 +0100

    refactor models and api for NodeEdit, PropertyEdit, RelationEdit

--- src/pages/graph/NodeEdit.tsx
+++ src/pages/graph/NodeEdit.tsx
@@ -1,14 +1,15 @@
 import React, { ChangeEvent, useContext, useEffect, useState } from 'react'
 import { makeStyles, TableCell, TableRow, TextField } from '@material-ui/core'
-import { GraphDelta, Node, Property } from '../../api/model/Model'
+import { GraphDelta, Node, Property } from '../../services/models'
 import { DomainSelect } from './DomainSelect'
 import { ColorPicker } from '../../components/ColorPicker/ColorPicker'
 import CustomTable from '../../components/Table/CustomTable'
 import Edit from './Edit'
 import GraphContext from '../../context/GraphContext'
-import graphService from '../../api/GraphService'
 import { SnackbarContext } from '../../context/Snackbar'
 import { Translations } from '../../utils/Translations'
+import { NodeApi } from '../../services'
+import AXIOS_CONFIG from '../../openapi/axios-config'
 
 interface NodeEditProps {
   node: Node
@@ -37,6 +38,7 @@ export const NodeEdit = (props: NodeEditProps): JSX.Element => {
   const [properties, setProperties] = useState<Property[]>([])
   const { openSnackbar, openSnackbarError } = useContext(SnackbarContext)
   const classes = useStyleTable()
+  const nodeApi = new NodeApi(AXIOS_CONFIG())
 
   const updateDomain = (newDomainIds: string[]): void => {
     setValue((oldNode) => ({ ...oldNode, domainIds: newDomainIds }))
@@ -48,26 +50,32 @@ export const NodeEdit = (props: NodeEditProps): JSX.Element => {
   }, [node])
 
   const onCreate = (newNode: Node): void => {
-    graphService.nodePost(newNode).then((graphDelta) => {
-      updateState(graphDelta)
-      setSelected(nodes.filter((n) => n.node.id === newNode.id)[0])
-    })
+    if (newNode.label)
+      nodeApi
+        .apiGraphNodePost({ ...newNode, label: newNode.label })
+        .then((graphDelta) => {
+          updateState(graphDelta.data)
+          setSelected(nodes.filter((n) => n.node.id === newNode.id)[0])
+        })
   }
   const onSubmit = (newNode: Node): void => {
-    graphService
-      .nodePut(newNode)
-      .then((graphDelta) => {
-        updateState(graphDelta)
-        setSelected(nodes.filter((n) => n.node.id === newNode.id)[0])
-      })
-      .then(() => openSnackbar(Translations.SAVE, 'success'))
-      .catch((e) => openSnackbarError(e))
+    console.log('in', newNode, { label: newNode.label, ...newNode })
+    if (newNode.id && newNode.label)
+      nodeApi
+        .apiGraphNodeIdPut(newNode.id, { label: newNode.label, ...newNode })
+        .then((graphDelta) => {
+          updateState(graphDelta.data)
+          setSelected(nodes.filter((n) => n.node.id === newNode.id)[0])
+        })
+        .then(() => openSnackbar(Translations.SAVE, 'success'))
+        .catch((e) => openSnackbarError(e))
   }
   const onDelete = (deleted: Node): void => {
-    graphService.nodeDelete(deleted.id).then((graphDelta) => {
-      updateState(graphDelta)
-      setSelected(undefined)
-    })
+    if (deleted.id)
+      nodeApi.apiGraphNodeIdDelete(deleted.id).then((graphDelta) => {
+        updateState(graphDelta.data)
+        setSelected(undefined)
+      })
   }
   const onClose = (): void => setSelected(undefined)
 
@@ -89,7 +97,7 @@ export const NodeEdit = (props: NodeEditProps): JSX.Element => {
           <TableRow className={classes.tableRow}>
             <TableCell className={classes.tableCell}>
               <ColorPicker
-                hex={value.color}
+                hex={value.color || '#6d6a6e'}
                 onChange={(hex: string): void => {
                   setValue((prevNode) => ({ ...prevNode, color: hex }))
                 }}
@@ -113,7 +121,7 @@ export const NodeEdit = (props: NodeEditProps): JSX.Element => {
         <DomainSelect
           domains={domains}
           label="Selected Domains"
-          valueDomainIds={value.domainIds}
+          valueDomainIds={Array.from(value.domainIds ?? [])}
           updateDomains={updateDomain}
         />
       </>
