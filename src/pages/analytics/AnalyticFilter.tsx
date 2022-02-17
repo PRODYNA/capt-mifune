485a9d7fc2018751c52d96f4ea8eab6359ffbe18
My Trang Hong <mytrang.hong@prodyna.com>
2022-02-17 15:55:09 +0100

    add datepicker, text/ number field, checkbox in analyticFilter depending on propertyTyp

--- src/pages/analytics/AnalyticFilter.tsx
+++ src/pages/analytics/AnalyticFilter.tsx
@@ -1,25 +1,41 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { IconButton, TableCell, useTheme } from '@material-ui/core'
+import {
+  Checkbox,
+  IconButton,
+  TableCell,
+  TextField,
+  useTheme,
+} from '@material-ui/core'
 import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox'
+import { DatePicker } from '@material-ui/pickers'
+import { formatISO } from 'date-fns'
+import { ParsableDate } from '@material-ui/pickers/constants/prop-types'
 import FormSelect from '../../components/Form/FormSelect'
 import { AnalyticSelect } from './AnalyticSelect'
 import { useStyleTable } from '../graph/NodeEdit'
 import ChartContext from '../../context/ChartContext'
 import { QueryFunction } from '../../services/models/query-function'
 import AXIOS_CONFIG from '../../openapi/axios-config'
-import { DataResourceApi } from '../../services'
+import { DataResourceApi, FilterFunction, PropertyType } from '../../services'
 
 interface AnalyticFilterProps {
   onKeyChange: (key?: string) => void
-  onValueChange: (key?: string) => void
+  onValueChange: (key?: string | number | boolean) => void
+  onFuncChange: (key?: FilterFunction) => void
   onDelete: () => void
 }
 
 export const AnalyticFilter = (props: AnalyticFilterProps): JSX.Element => {
-  const { onDelete, onKeyChange, onValueChange } = props
+  const { onDelete, onKeyChange, onValueChange, onFuncChange } = props
   const { query } = useContext(ChartContext)
   const [filter, setFilter] = useState<string | undefined>(undefined)
-  const [value, setValue] = useState<string>()
+  const [func, setFunc] = useState<FilterFunction>(FilterFunction.Equal)
+  const [propertyType, setPropertyType] = useState<PropertyType | undefined>(
+    undefined
+  )
+  const [value, setValue] = useState<string | number | boolean | undefined>(
+    undefined
+  )
   const [values, setValues] = useState<string[]>()
   const theme = useTheme()
   const classes = useStyleTable()
@@ -65,12 +81,19 @@ export const AnalyticFilter = (props: AnalyticFilterProps): JSX.Element => {
     }
   }, [filter])
 
+  useEffect(() => {
+    if (func !== FilterFunction.Equal) {
+      if (propertyType === PropertyType.Boolean) setValue(true)
+    }
+  }, [func, propertyType])
+
   return (
     <>
       <AnalyticSelect
         renderAsTable
         label="Property"
         query={query}
+        setPropertyType={setPropertyType}
         onChange={(newFilter) => {
           if (newFilter) {
             setFilter(newFilter[0])
@@ -85,14 +108,80 @@ export const AnalyticFilter = (props: AnalyticFilterProps): JSX.Element => {
         <FormSelect
           title=""
           hideLabel
-          options={values ?? []}
-          value={value ?? ''}
+          options={Object.values(FilterFunction) ?? []}
+          value={func ?? ''}
           onChangeHandler={(event) => {
-            setValue(event.target.value)
-            onValueChange(event.target.value)
+            setFunc(event.target.value)
+            onFuncChange(event.target.value)
           }}
         />
       </TableCell>
+      {func === FilterFunction.Equal && (
+        <TableCell className={classes.tableCell}>
+          <FormSelect
+            title=""
+            hideLabel
+            options={values ?? []}
+            value={(value as string) ?? ''}
+            onChangeHandler={(event) => {
+              setValue(event.target.value)
+              onValueChange(event.target.value)
+            }}
+          />
+        </TableCell>
+      )}
+      {func !== FilterFunction.Equal && (
+        <TableCell className={classes.tableCell}>
+          {propertyType === PropertyType.Date && (
+            <DatePicker
+              autoOk
+              label=""
+              views={['year', 'month', 'date']}
+              value={(value as ParsableDate) ?? null}
+              format="dd.MM.yyyy"
+              onChange={(date) => {
+                const formatDate = formatISO(date as Date, {
+                  representation: 'date',
+                })
+                setValue(formatDate)
+                onValueChange(formatDate)
+              }}
+              animateYearScrolling
+            />
+          )}
+          {propertyType === PropertyType.Boolean && (
+            <Checkbox
+              checked={(value as boolean) ?? true}
+              onChange={(event) => {
+                setValue(event.target.checked)
+                onValueChange(event.target.checked)
+              }}
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+          )}
+          {propertyType !== PropertyType.Date &&
+            propertyType !== PropertyType.Boolean && (
+              <TextField
+                type={propertyType === PropertyType.String ? 'text' : 'number'}
+                value={
+                  (propertyType === PropertyType.String
+                    ? (value as string)
+                    : (value as number)) ?? ''
+                }
+                onChange={(event) => {
+                  let newValue
+                  if (propertyType === PropertyType.String) {
+                    newValue = event.target.value as string
+                  } else {
+                    newValue = parseInt(event.target.value, 10) as number
+                  }
+                  setValue(newValue)
+                  onValueChange(newValue)
+                }}
+              />
+            )}
+        </TableCell>
+      )}
       <TableCell width={30}>
         <IconButton onClick={onDelete} style={{ padding: 0 }}>
           <IndeterminateCheckBoxIcon htmlColor={theme.palette.error.main} />
