24a1e7e43959613193e38bd337bfb32574a34d2e
readmey <mytrang.hong@hotmail.de>
2023-10-18 15:39:30 +0200

    remove unused code

--- ui/src/components/Error/ErrorBoundaries.tsx
+++ ui/src/components/Error/ErrorBoundaries.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import ErrorMessage from './ErrorMessage'
 
 interface ErrorBoundaryState {
